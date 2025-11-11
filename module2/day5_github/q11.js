fe95df3a015dd46df2c44a83a5dd3784449c0dd0
abvenu <avenugopalb@gmail.com>
2025-11-11 16:21:34 +0530

    Step 1 is done

--- module2/day5_github/q11.js
+++ module2/day5_github/q11.js
@@ -1,9 +1,20 @@
 
 
-let num = 2;
+let num = 15;
 
-if(num%2==0){
-    console.log("Even")
-}else{
-    console.log("Odd")
+function checkPrime(num){
+    let count = 0
+    for(let i=1;i<=num;i++){
+        if(num%i==0){
+            count++
+        }
+    }
+
+    if(count==2){
+        return true
+    }else{
+        return false
+    }
 }
+
+console.log(checkPrime(num))
